a56a3df98060810abf7923a5a158340e6938b4e3
gitconfig <katsuhiko.nonomura@gmail.com>
2020-10-12 08:18:54 +0900

    add webapi for GridDB V4.5

---
+++ webapi/4.5/sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -w '\n' -X POST --basic -u admin:admin -H "Content-type:application/json" -d '{"container_name":"test", "container_type":"COLLECTION", "rowkey":true, "columns":[{"name":"col1", "type":"STRING", "index":["TREE"]}, {"name":"col2", "type":"INTEGER"}, {"name":"col3", "type":"BOOL"}]}' http://127.0.0.1:8080/griddb/v2/dockerGridDB/dbs/public/containers
+
+curl -w '\n' -X PUT --basic -u admin:admin -H "Content-type:application/json" -d '[["value", 1, true]]' http://127.0.0.1:8080/griddb/v2/dockerGridDB/dbs/public/containers/test/rows 
+
+curl -w '\n' -X POST --basic -u admin:admin -H "Content-type:application/json" -d '{"limit":1000}' http://127.0.0.1:8080/griddb/v2/dockerGridDB/dbs/public/containers/test/rows 
+
+curl -w '\n' -X POST --basic -u admin:admin -H "Content-type:application/json" -d '[{"name":"test", "stmt":"select *", "columns":[]}]' http://127.0.0.1:8080/griddb/v2/dockerGridDB/dbs/public/tql 
+
+curl -w '\n' -X POST --basic -u admin:admin -H "Content-type:application/json" -d '[{"type":"sql-select", "stmt":"select * from test"}]' http://127.0.0.1:8080/griddb/v2/dockerGridDB/dbs/public/sql 
